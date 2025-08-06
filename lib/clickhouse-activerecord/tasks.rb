2e3585e61f0cf2e553c3a2f11d326c36b806c2c6
Tyler Rhodes <rhodetyl000@gmail.com>
2025-08-06 14:08:49 -0700

    Use old sorting behavior for clickhouse_structure.sql Define mat views above tables

--- lib/clickhouse-activerecord/tasks.rb
+++ lib/clickhouse-activerecord/tasks.rb
@@ -34,28 +34,27 @@ module ClickhouseActiverecord
       create
     end
 
-    def structure_dump(*args)
+    def structure_dump(path, *)
       establish_master_connection
 
-      # get all tables
-      tables = connection.execute("SHOW TABLES FROM #{@configuration.database} WHERE name NOT LIKE '.inner_id.%'")['data'].flatten.map do |table|
-        connection.show_create_table(table, single_line: false).gsub("#{@configuration.database}.", '')
-      end.compact
+      functions = connection.execute("SELECT create_query FROM system.functions WHERE origin = 'SQLUserDefined' ORDER BY name")['data']
+                            .flatten
+                            .map { |function| function.gsub('\\n', "\n") }
+      functions = connection.execute("SELECT create_query FROM system.functions WHERE origin = 'SQLUserDefined' ORDER BY name")['data']
+        .flatten
+        .map { |function| function.gsub('\\n', "\n") }
+      table_defs = connection.execute("SHOW TABLES FROM #{@configuration.database}")['data']
+        .flatten
+        .reject { |name| /\.inner/.match?(name) || %w[schema_migrations ar_internal_metadata].include?(name) }
+        .map { |name| connection.show_create_table(name, single_line: false).gsub("#{@configuration.database}.", '') }
+      views, tables = table_defs.partition { |sql| sql.match(/^CREATE\s+(MATERIALIZED\s+)?VIEW/) }
+      mat_views, views = views.partition { |sql| sql.match(/^CREATE\s+MATERIALIZED\s+VIEW/) }
 
-      # sort view to last
-      tables.sort_by! {|table| table.match(/^CREATE\s+(MATERIALIZED\s+)?VIEW/) ? 1 : 0}
+      definitions = functions.sort + mat_views.sort + tables.sort + views.sort
 
-      # get all functions
-      functions = connection.execute("SELECT create_query FROM system.functions WHERE origin = 'SQLUserDefined' ORDER BY name")['data'].flatten
-
-      # put to file
-      File.open(args.first, 'w:utf-8') do |file|
-        functions.each do |function|
-          file.puts function.gsub('\\n', "\n") + ";\n\n"
-        end
-
-        tables.each do |table|
-          file.puts table + ";\n\n"
+      File.open(path, 'w:utf-8') do |file|
+        definitions.each do |sql|
+          file.puts "#{sql};\n\n"
         end
       end
     end
