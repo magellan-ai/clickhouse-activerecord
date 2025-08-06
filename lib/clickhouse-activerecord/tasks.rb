64539aa8207b10f2384ae04e28c79b7cdf7879fa
Tyler Rhodes <rhodetyl000@gmail.com>
2025-08-06 15:47:19 -0700

    Define mat views above tables

--- lib/clickhouse-activerecord/tasks.rb
+++ lib/clickhouse-activerecord/tasks.rb
@@ -47,7 +47,9 @@ module ClickhouseActiverecord
         .reject { |name| /\.inner/.match?(name) || %w[schema_migrations ar_internal_metadata].include?(name) }
         .map { |name| connection.show_create_table(name, single_line: false).gsub("#{@configuration.database}.", '') }
       views, tables = table_defs.partition { |sql| sql.match(/^CREATE\s+(MATERIALIZED\s+)?VIEW/) }
-      definitions = functions.sort + tables.sort + views.sort
+      mat_views, views = views.partition { |sql| sql.match(/^CREATE\s+MATERIALIZED\s+VIEW/) }
+
+      definitions = functions.sort + mat_views.sort + tables.sort + views.sort
 
       File.open(path, 'w:utf-8') do |file|
         definitions.each do |sql|
