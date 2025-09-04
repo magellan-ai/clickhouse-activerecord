c294f87349a466d4653ac6dbaf3fcdacba2c52f8
Ryan Kerr <leboshi@gmail.com>
2025-09-04 16:03:37 -0400

    Fix function dump from bad rebase.

--- lib/active_record/connection_adapters/clickhouse/schema_statements.rb
+++ lib/active_record/connection_adapters/clickhouse/schema_statements.rb
@@ -121,7 +121,7 @@ module ActiveRecord
         def show_create_function(function)
           result = do_system_execute("SELECT create_query FROM system.functions WHERE origin = 'SQLUserDefined' AND name = '#{function}'")
           return if result.nil?
-          result['data'].flatten.first
+          result['data'].flatten.first.sub(/\ACREATE FUNCTION/, 'CREATE OR REPLACE FUNCTION')
         end
 
         def table_options(table)
