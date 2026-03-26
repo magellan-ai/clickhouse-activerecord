7be2be642a0ccd453665690b1f469964d6a759aa
Tyler Rhodes <rhodetyl000@gmail.com>
2026-03-26 13:27:36 -0700

    Pass tests locally
    With exception to `primary_key` shim related specs. This must come after
    we deploy to Rails 8.

--- lib/arel/visitors/clickhouse.rb
+++ lib/arel/visitors/clickhouse.rb
@@ -66,7 +66,7 @@ module Arel
         collector << "SETTINGS "
         o.expr.each_with_index do |(key, value), i|
           collector << ", " if i > 0
-          collector << key.to_s.gsub(/\W+/, "")
+          collector << sanitize_as_setting_name(key).to_s
           collector << " = "
           collector << sanitize_as_setting_value(value)
         end
@@ -112,7 +112,7 @@ module Arel
 
       def sanitize_as_setting_name(value)
         return value if Arel::Nodes::SqlLiteral === value
-        @connection.sanitize_as_setting_name(value)
+        value.to_s.gsub(/\W+/, "")
       end
 
       private
